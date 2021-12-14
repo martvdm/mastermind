49c6ef96b9b096204d7b242cc2e2d22cc163fb41
Student Mart van der Molen <mmolen1288@student.landstede.nl>
2021-12-14 09:38:10 +0100

    checkstage bugfixes

--- app/Http/Controllers/checkstageController.php
+++ app/Http/Controllers/checkstageController.php
@@ -16,57 +16,62 @@ class checkstageController extends Controller
     ///
     ///    If color variable does not exist:
     ///    !Selected id = _
-    ///
+    ///w
     ///     Pushes selected color id into Array playboard.
 ###############################################
-public function check() {
+    public function check()
+    {
 ##############################################
 
-    // The list of vars down here import all variables needed for this funtion.
-    $randomgameid = Session::get('randomgameid'); #The random gameID and asnwer secret code
-    $playboard = Session::get('playboard'); #In this array the hole playboard input will be stored
-    $playboardcheck = Session::get('playboardcheck');
-    $currentstageindex = Session::get('currentstageindex'); #The current stage where the game is in
-    $victory = false;
-    $filledcells = 0; #Start of check if the stage is fully filled with integers.
+        // The list of vars down here import all variables needed for this funtion.
+        $randomgameid = Session::get('randomgameid'); #The random gameID and asnwer secret code
+        $playboard = Session::get('playboard'); #In this array the hole playboard input will be stored
+        $playboardcheck = Session::get('playboardcheck');
+        $currentstageindex = Session::get('currentstageindex'); #The current stage where the game is in
+        $victory = false;
+        $filledcells = 0; #Start of check if the stage is fully filled with integers.
 
-foreach ($playboard[$currentstageindex] as $singlecellcheck) {
-    if ($singlecellcheck != null) {
-    $filledcells = $filledcells + 1;
-    }
-}
-
-    if(count($playboard[$currentstageindex]) == count(array_keys($playboard[$currentstageindex]))) {
-        $index = -1;
-        foreach ($playboard[$currentstageindex] as $singlecell) {
-            $index++;
-            if (in_array($playboard[$currentstageindex][$index], $randomgameid) === true) {
-                $playboardcheck[$currentstageindex][$index] = 1; # staat voor bevatten
-                if ($playboard[$currentstageindex][$index] == $randomgameid[$index]) {
-                    $playboardcheck[$currentstageindex][$index] = 2; # staat voor Goed
-                    $victory = true;
-                }
-            } else {
-                $playboardcheck[$currentstageindex][$index] = 0; # 0 staat voor fout
+        foreach ($playboard[$currentstageindex] as $singlecellcheck) {
+            if ($singlecellcheck != null) {
+                $filledcells = $filledcells + 1;
             }
         }
-        if ($randomgameid == $playboard[$currentstageindex]) {
+
+        if (count($playboard[$currentstageindex]) == count(array_keys($playboard[$currentstageindex]))) {
+            $index = -1;
+            foreach ($playboard[$currentstageindex] as $singlecell) {
+                $index++;
+                if (in_array($singlecell, $randomgameid)) {
+                    $playboardcheck[$currentstageindex][$index] = 1; # staat voor bevatten
+
+                    
+                    if ($singlecell == $randomgameid[$index]) {
+                        $playboardcheck[$currentstageindex][$index] = 2; # staat voor Goed
+                        $victory = true;
+                    }
+                } else {
+                    $playboardcheck[$currentstageindex][$index] = 0; # 0 staat voor fout
+                }
+            }
+            if ($randomgameid == $playboard[$currentstageindex]) {
+                Session::forget('');
+                Session::flush();
+            }
+
+        } elseif (9 > $currentstageindex) {
             Session::forget('');
             Session::flush();
         }
+        if (Session::has('randomgameid') && $filledcells == 4) {
+            $currentstageindex = $currentstageindex + 1;
+            Session::put('currentstageindex', $currentstageindex);
+            Session::put('playboardcheck', $playboardcheck);
+        }
 
-    } elseif (9 > $currentstageindex) {
-        Session::forget('');
-        Session::flush();
+
+        return redirect('/testapps/gameinput');
+
+
     }
-    if (Session::has('randomgameid') && $filledcells == 4) {
-    $currentstageindex = $currentstageindex + 1;
-    Session::put('currentstageindex', $currentstageindex);
-    Session::put('playboardcheck', $playboardcheck);}
-
-
-return redirect('/testapps/gameinput');
-
-}
 
 }
